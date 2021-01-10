7269d5c3043e35dd7ebf678ae68126547014176d
Eduar Ursu <eduardursu78@gmail.com>
2021-01-10 15:26:58 +0100

    Update gWallpaper #1.1

--- src/gWallpaper.user.js
+++ src/gWallpaper.user.js
@@ -3,10 +3,16 @@
 // @namespace   Scriptz (https://github.com/d3ward/scriptz)
 // @include     http://*.google.*/*
 // @include     https://*.google.*/*
+// @exclude http://www.google.*/reader*
+// @exclude https://www.google.*/reader*
+// @exclude http://www.google.*/calendar*
+// @exclude https://www.google.*/calendar*
+// @exclude http://www.google.*/ig*
+// @exclude https://www.google.*/ig*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Eduard Ursu ( d3ward )
-// @description Change Google Search Engine background image every day with random wallpaper 
+// @description Change Google Search Engine background image every timeE with random wallpaper + UI improvements
 // ==/UserScript==
 
 //Edit the above list with images url
@@ -27,16 +33,29 @@ function getWallpaper() {
     return wallpaperList[ num ];
 }
 
-// Checks if one day has passed. 
-function hasOneDayPassed(){
-    var date = new Date().toLocaleDateString();
-    if( localStorage.imageDate == date ) return false;
-    localStorage.imageDate = date;
-    return true;
+/* Set time Elapsed in seconds
+ * 1 minute  = 60
+ * 1 h = 3600
+ * 1 day = 86400
+ * 1 week = 604800
+*/
+var timeE = 604800;
+// Checks time elapsed 
+function hasTimePassed(){
+  
+  
+    var date = new Date();
+    var date2 = new Date(localStorage.imageDate)
+    var timeDiff= date - date2;
+    timeDiff = Math.round(timeDiff/1000);
+  
+    if( timeDiff > timeE ){localStorage.imageDate = date; return true; }
+      
+    return false;
 }
 //Update image url once per day 
 function runOncePerDay(){
-    if( !hasOneDayPassed() ) return false;
+    if( !hasTimePassed() ) return false;
     localStorage.imageUrl = getWallpaper();
 }
 
@@ -46,3 +65,12 @@ var url=localStorage.imageUrl;
 var b = document.body;
 b.style.background = '#ccc url("'+url+'") no-repeat center center fixed';
 b.style.backgroundSize = "cover";
+
+function addCSS() {
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.g{width: auto!important;background: var(--darkreader-neutral-background ,#fff)!important;border-radius: 8px!important;padding: 10px!important;}.gb_Hg.gb_Ta{margin-right:10px!important;}#taw,#botstuff,#foot,.gb_Hg.gb_Ta,g-card,.Xeztj{background-color: var(--darkreader-neutral-background ,#fff)!important;border-radius:8px;padding:10px;}#foot,#rhs .g-blk.VjDLd,#rhs .VjDLd.liYKde,#taw{margin:20px 0;}#center_col .kp-blk{width:auto!important;}';
+    document.head.appendChild(style);
+}
+addCSS();
+
